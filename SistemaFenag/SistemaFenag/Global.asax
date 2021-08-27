4a705465d3a6709b1b847c3562f85055ff674a4a
Rafael Pereira <62318484+banguelajpg@users.noreply.github.com>
2021-08-27 02:53:34 -0300

    Remover projetos

--- SistemaFenag/SistemaFenag/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaFenag.Global" Language="C#" %>
--- WebApp_vbnet/WebApp_vbnet/Scripts/Paginas/JavaScript.js
+++
@@ -1,8 +0,0 @@
-﻿let Variavel1
-
-
-
-$(document).ready(function () {
-    // document is loaded and DOM is ready
-    alert("document is ready");
-});
